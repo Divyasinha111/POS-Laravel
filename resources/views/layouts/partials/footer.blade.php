9211ea674b522e9ac69473289db119d46936dbe9
Divyasinha111 <divyasinha5482@gmail.com>
2025-04-24 21:12:47 +0530

    first commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      Developed By Divya sinha and Riyajeet singh.
+    </div>
+    <strong>&copy; <?php echo date('Y')?> - Point of Sale System</strong> - PHP Laravel
+  </footer>
